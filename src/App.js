91b795e15f844759f29f8d549b5ea3b8f84c0419
karantestfly <137740837+karantestfly@users.noreply.github.com>
2023-07-07 15:45:47 +0530

    screen shoot disabled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import { useEffect } from "react";
 import "./App.css";
 import HeaderContainer from "./components/HeaderContainer/HeaderContainer";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
+  useEffect(() => {
+    const preventScreenshot = () => {
+      document.addEventListener('keydown', handleKeyDown);
+      document.addEventListener('keyup', handleKeyUp);
+    };
+
+    const handleKeyDown = (e) => {
+      if (e.ctrlKey || e.metaKey) {
+        e.preventDefault();
+        alert('Screenshots are not allowed on this page.');
+      }
+    };
+
+    const handleKeyUp = (e) => {
+      if (e.ctrlKey || e.metaKey) {
+        e.preventDefault();
+      }
+    };
+
+    preventScreenshot();
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('keyup', handleKeyUp);
+    };
+  }, []);
+
+  const handleContextMenu = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
+    <div onContextMenu={handleContextMenu}>
       <Navbar />
       <HeaderContainer />
     </div>
